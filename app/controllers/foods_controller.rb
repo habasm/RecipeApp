3f55760270505ebce9ae42b7231a82888a0233f7
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-05-31 13:19:01 +0530

    updated styling

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,21 +1,13 @@
 class FoodsController < ApplicationController
-  before_action :authenticate_user!
-  def index; end
+  def index
+    @foods = Food.all
+  end
 
   def show
-    if params[:id] == 'sign_out'
-      logout_user
-    else
-      set_user
-    end
+   
   end
 
   def new; end
 
-  private
 
-  def logout_user
-    sign_out current_user
-    redirect_to new_user_session_path
-  end
 end
