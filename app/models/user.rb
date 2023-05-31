63ab607fd98e4a9b1bf7da1081abca9ae2856dec
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-05-31 13:55:54 +0530

    fixed linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
-
 end
