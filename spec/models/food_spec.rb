ddc0a56fc60c80a5ace31abb65f00284481d2ede
juharyimami <juharyimami2011@gmail.com>
2023-06-01 15:26:03 +0300

    food model tested

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(name: 'Wise', email: 'wise@gmail.com', password: 'password123')
+    @food = Food.create(name: 'chicken', user: @user, price: 5, measurement_unit: 'kg', quantity: 4, user_id: @user.id)
+  end
+
+  it 'checks if food is successfully created' do
+    expect(@food).to be_valid
+  end
+
+  it 'checks it is not valid if user is not present' do
+    @food.user = nil
+    expect(@food).to_not be_valid
+  end
+
+  it 'checks it is not created successfully without a name' do
+    @food.name = nil
+    expect(@food).to_not be_valid
+  end
+
+  it 'checks it is not successfully created without a quantity' do
+    @food.quantity = nil
+    expect(@food).to_not be_valid
+  end
+
+  it 'checks if parameters are exactly what is saved ' do
+    expect(@food.name).to eql 'chicken'
+  end
 end
