8a80c2a179c56458aedd19c9bb4714d4c178e766
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-06-01 06:57:22 +0530

    fixed linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   validates :name, presence: true
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
   has_many :recipes, dependent: :destroy
